b8578c738401d8574a59eb8424189e69a7b83cc6
wildcard329 <enkiduwildman@gmail.com>
2020-01-23 13:21:42 -0800

    added some styling

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -9,10 +9,15 @@ import {
 const PictureCard = props => {
     return (
         <div>
-            <h2>{props.title}</h2>
+            {/* <h2>{props.title}</h2>
             <h3>Date: {props.date}</h3>
             <p>Description: {props.explanation}</p>
-            <img src={props.url} alt=""/>
+            <img src={props.url} alt=""/> */}
+            <Card>
+                <CardTitle color = "yellow">{props.title}</CardTitle>
+                <CardSubtitle>{props.date}</CardSubtitle>
+                <CardText>{props.explanation}</CardText>
+            </Card>
             
         </div> 
     );
