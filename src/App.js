ce43f2e000e88e1418bb9a257ccc0efa2c907066
wildcard329 <enkiduwildman@gmail.com>
2020-01-23 15:03:23 -0800

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import InformationCard from "./components/InformationCard"
+import InformationCard from "./components/InformationCard";
+import ReactStrap from "reactstrap";
 import "./App.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: "black", color: "white"}}>
       <h1>Nasa Photo of the Day</h1>
-      <InformationCard/>
+      < InformationCard/>
     </div>
   );
 }
--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Card,
     CardText,
     CardTitle,
-    CardSubtitle
+    CardSubtitle,
+    CardImg
 } from "reactstrap";
 
 const PictureCard = props => {
+    
     return (
         <div>
             {/* <h2>{props.title}</h2>
             <h3>Date: {props.date}</h3>
             <p>Description: {props.explanation}</p>
             <img src={props.url} alt=""/> */}
-            <Card>
-                <CardTitle color = "yellow">{props.title}</CardTitle>
+            <Card style={{backgroundColor: "gray", color: "yellow"}}>
+                <CardTitle>{props.title}</CardTitle>
                 <CardSubtitle>{props.date}</CardSubtitle>
                 <CardText>{props.explanation}</CardText>
+                <CardImg src={props.url} alt='' />
             </Card>
             
         </div> 
