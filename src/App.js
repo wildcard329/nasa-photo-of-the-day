835c057a134912e6720b64f646f68551c24b7c4a
wildcard329 <enkiduwildman@gmail.com>
2020-01-23 15:30:24 -0800

    added toggle feature

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import InformationCard from "./components/InformationCard";
 import ReactStrap from "reactstrap";
 import "./App.css"
 
+
 function App() {
   return (
     <div className="App" style={{backgroundColor: "black", color: "white"}}>
-      <h1>Nasa Photo of the Day</h1>
+      <h1>NASA Photo of the Day</h1>
       < InformationCard/>
     </div>
   );
--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -4,24 +4,32 @@ import {
     CardText,
     CardTitle,
     CardSubtitle,
-    CardImg
+    CardImg,
+    Collapse,
+    Button,
+    CardBody
 } from "reactstrap";
 
 const PictureCard = props => {
-    
+
+    const [isOpen, setIsOpen] =useState(false);
+    const toggle = () =>setIsOpen(!isOpen);
+
     return (
         <div>
-            {/* <h2>{props.title}</h2>
-            <h3>Date: {props.date}</h3>
-            <p>Description: {props.explanation}</p>
-            <img src={props.url} alt=""/> */}
-            <Card style={{backgroundColor: "gray", color: "yellow"}}>
-                <CardTitle>{props.title}</CardTitle>
-                <CardSubtitle>{props.date}</CardSubtitle>
-                <CardText>{props.explanation}</CardText>
-                <CardImg src={props.url} alt='' />
-            </Card>
-            
+            <button color="black" backgroundColor="black" onClick={toggle} style={{marginBottom: '1rem'}}>CLICK</button>
+            <Collapse isOpen={isOpen}>
+                <Card>
+                    <CardBody>
+                        <Card style={{backgroundColor: "gray", color: "yellow"}}>
+                            <CardTitle>{props.title}</CardTitle>
+                            <CardSubtitle>{props.date}</CardSubtitle>
+                            <CardText>{props.explanation}</CardText>
+                            <CardImg src={props.url} alt='' />
+                        </Card>     
+                    </CardBody>
+                </Card>
+            </Collapse>
         </div> 
     );
 };
